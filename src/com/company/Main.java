6db957803601003f9a6a83f6a241c9141519b3e2
unknown <magurarm@guilford.edu>
2020-06-26 17:05:25 -0400

    Feat: Add loadItems method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         //code here
+        Simulation s1 = new Simulation();
+        File file = new File("phase-1.txt");
+
+        ArrayList items = s1.loadItems(file);
+        for(Object item: items){
+            System.out.println(item);
+        }
+//        System.out.println(Arrays.deepToString(items.toArray()));
+//        System.out.println(items.get(0).toString());
     }
 }
--- src/com/company/Simulation.java
+++ src/com/company/Simulation.java
@@ -1,4 +1,34 @@
 package com.company;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Scanner;
+import java.io.File;
+
 
 public class Simulation {
-}
+
+    public ArrayList loadItems(File file){
+    ArrayList<Item> list = new ArrayList<Item>();
+    Scanner scanner;
+    {
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                for (int i=0; i<1; i++) {
+                    Item item = new Item();
+                    String[] array = line.split("=");
+                    item.name = array[i];
+                    item.weight = Integer.parseInt(array[i+1]);
+                    list.add(item);
+                }
+            }
+        }
+         catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Sorry, could not find file");
+        }
+    }
+        return list;
+    }}
+
