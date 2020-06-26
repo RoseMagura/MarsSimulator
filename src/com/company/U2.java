6b7289c6e68f5328740328d2709746c88cfc6ef5
unknown <magurarm@guilford.edu>
2020-06-26 15:42:57 -0400

    Edit: edit U1 and U2 methods to fix bugs

--- src/com/company/U2.java
+++ src/com/company/U2.java
@@ -2,17 +2,17 @@ package com.company;
 
 public class U2 extends Rocket {
     int cost = 120000000; //in US dollars
-    int weight = 18; //in tons
+    int currentWeight = 18; //in tons
     int maxWeight = 29; //in tons
-    //odds of exploding during launch
-    public double explosionChance = 0.04 * (double)(weight/maxWeight);
-    //odds of exploding during landing
-    public double crashChance = 0.08 * (double)(weight/maxWeight);
     public boolean launch(){
+        //odds of exploding during launch
+        double explosionChance = 0.04 * (double)(currentWeight/maxWeight);
         int random = (int) (Math.random() * 100) + 1;
         return random > explosionChance;
     }
     public boolean land() {
+        //odds of exploding during landing
+        double crashChance = 0.08 * (double)(currentWeight/maxWeight);
         int random2 = (int) (Math.random() * 100) + 1;
         return random2 > crashChance;
     }
